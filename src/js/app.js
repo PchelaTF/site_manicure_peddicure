a7d813548efc392420a8902c3806a2189c23fc55
PchelaTF <PchelaTF@gmail.com>
2022-07-30 23:11:47 +0300

    Done adaptive

--- src/js/app.js
+++ src/js/app.js
@@ -9,32 +9,40 @@ const workSlider = new Swiper('.work__slider', {
     slidesPerView: 1,
     loop: true,
     modules: [Navigation, Pagination],
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-    },
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
     },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    spaceBetween: 20,
+    setWrapperSize: true,
     breakpoints: {
         1170: {
             slidesPerView: 3,
             slidesPerGroup: 3,
             spaceBetween: 94,
+            pagination: {
+                el: '.swiper-pagination',
+                clickable: true,
+            },
         },
-        991.87: {
-            slidesPerView: 2.5,
-            slidesPerGroup: 2.5,
-            spaceBetween: 95,
-        }
-    }
+        767.98: {
+            slidesPerView: 2,
+            slidesPerGroup: 2,
+            // spaceBetween: 80,
+            spaceBetween: 30,
+            setWrapperSize: true,
+        },
+    },
 });
 
 const sliderReviews = new Swiper('.reviews__slider', {
-    slidesPerView: 2,
-    slidesPerGroup: 2,
-        // loop: true,  
+    slidesPerView: 1,
+    slidesPerGroup: 1,
+    // loop: true,  
     modules: [Navigation, Pagination],
     pagination: {
         el: '.swiper-pagination',
@@ -45,6 +53,27 @@ const sliderReviews = new Swiper('.reviews__slider', {
         prevEl: '.swiper-button-prev',
     },
     spaceBetween: 34,
+    setWrapperSize: true,
+    breakpoints: {
+        1170: {
+            slidesPerView: 2,
+            slidesPerGroup: 2,
+            spaceBetween: 34,
+        },
+        767.98: {
+            slidesPerView: 1,
+            slidesPerGroup: 1,
+            slidesOffsetBefore: 50,
+        },
+        564.98: {
+            slidesPerView: 1,
+            slidesPerGroup: 1,
+            slidesOffsetBefore: 33,
+        },
+        413.98: {
+            slidesOffsetBefore: 65,
+        }
+    },
 });
 
 
